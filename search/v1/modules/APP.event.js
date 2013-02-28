b328ac82568ec3dc38b7a590863dbf363142215d
Adrian Statescu <mergesortv@gmail.com>
2013-02-28 12:42:24 -0800

    updated

--- search/v1/modules/APP.event.js
+++ search/v1/modules/APP.event.js
@@ -4,7 +4,7 @@
  */
 
 
-var event = function(){
+var _event = function(){
 
          return {  
 
@@ -83,10 +83,16 @@ var event = function(){
 
 HTMLElement.prototype.Click = function( fn ) {
 
-       event.bind(this, 'click', fn, false)
-} 
+       _event.bind(this, 'click', fn, false)
+};
 
-    define("event", [], function(){
+HTMLElement.prototype.Submit = function( fn ) {
 
-                  return event
-    });
+       _event.bind(this, 'submit', fn, false)
+} ;
+
+
+define("event", [], function(){
+
+              return _event;
+});
