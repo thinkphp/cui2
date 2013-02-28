16cc207084bf51a1f2ea6d2af78bbb4e69cfc61c
Adrian Statescu <mergesortv@gmail.com>
2013-02-27 20:15:05 -0800

    google search version 1 and 2 are ready

---
+++ search/v2/router.js
@@ -0,0 +1,98 @@
+function Router() {
+
+         var self = this;
+
+         this._compiled = [];
+
+         this._rules = [];
+
+         var attach = function() {
+
+             window.addEventListener('hashchange', function(){
+
+                    self._match(document.location.hash)
+             })
+         };
+
+         attach();   
+};
+
+Router.prototype._compile = function() {
+
+       var n = this._rules.length,
+
+           i;
+
+
+       for(i = 0; i < n; i++) {
+
+           var keys = [];
+
+           var reg = this._rules[ i ][ 0 ].replace(/:[^\/]+/gi, function( key ){
+
+               keys.push( key.substring(1) )
+             
+               return "([^\/]+)"
+           })
+
+
+           this._compiled.push({
+
+                 keys: keys,
+
+                 handler: this._rules[ i ] [ 1 ],
+
+                 re: new RegExp( reg )
+           })   
+
+
+       }
+
+};
+
+Router.prototype._match = function( url ) {
+
+       var data = {},
+
+           n = this._compiled.length,
+
+           i,
+
+           j;
+
+           for(i = 0; i < n; i++) {
+
+               var match = url.match(this._compiled[ i ].re);
+
+               if( match ) {
+
+                    var keys = this._compiled[ i ].keys,
+
+                        m = keys.length,
+
+                        handler = this._compiled[i].handler;
+
+                        for(j = 0; j < m; j++) {
+
+                            data[ keys[ j ] ] = match[j+1];
+                        } 
+
+                        if( handler ) {
+
+                            handler( data )
+                        }
+
+               }
+           }   
+
+};
+
+Router.prototype.get = function(rule, handler) {
+
+       this._rules.push([rule, handler])
+};
+
+Router.prototype.run = function() {
+
+       this._compile()
+};
